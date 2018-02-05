3493df5a7747ad72976de9dabd10c1b1749080e6
Edi_Edoy <edimariyanto491@gmail.com>
2018-02-05 21:08:23 +0700

    add tutor menu with modify main menu tutor

---
+++ app/src/main/java/com/example/user/tutor/MainTutor.java
@@ -0,0 +1,24 @@
+package com.example.user.tutor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainTutor extends AppCompatActivity {
+
+    ListView listView;
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_tutor);
+
+        listView = (ListView) findViewById(R.id.listMain);
+        String [] listMenu = getResources().getStringArray(R.array.lisMenu);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listMenu);
+        listView.setAdapter(adapter);
+    }
+}
