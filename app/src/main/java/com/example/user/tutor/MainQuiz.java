5f3fac21d8579a9778950b36c4ea93022a752a55
Edi_Edoy <edimariyanto491@gmail.com>
2018-02-05 22:59:19 +0700

    create module Quiz

---
+++ app/src/main/java/com/example/user/tutor/MainQuiz.java
@@ -0,0 +1,13 @@
+package com.example.user.tutor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainQuiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_quiz);
+    }
+}
